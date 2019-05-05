0f019918f2a0539e3f240c93505bd7b1b1d52e9a
Nolan Neeley <nneeley@futurefinance.com>
2019-05-05 13:24:35 -0500

    add title at start

--- session.rb
+++ session.rb
@@ -20,9 +20,19 @@ class Session
   private
 
   def setup
-    select_book
+    display_info
+    welcome
     select_branch
+    select_book
     is_available?
+  end
+
+  def display_info
+    puts INFO
+  end
+
+  def welcome
+    puts "You\'ll pick a library branch and book and Librarian tells you if it's available."
   end
 
   def select_book
@@ -44,4 +54,22 @@ class Session
   def is_available?
     book_status.availability(isbn, branch)
   end
+
+INFO = <<Doc
+*********************************************************************************************
+
+
+888 ,e, 888
+888  "  888-~88e  888-~\   /~~~8e  888-~\ Y88b  /         /~~~8e  888-~88e 888-~88e  e88~~8e
+888 888 888  888b 888          88b 888     Y888/              88b 888  888 888  888 d888  88b
+888 888 888  8888 888     e88~-888 888      Y8/          e88~-888 888  888 888  888 8888__888
+888 888 888  888P 888    C888  888 888       Y          C888  888 888  888 888  888 Y888    ,
+888 888 888-_88"  888     "88_-888 888      /            "88_-888 888  888 888  888  "88___/
+                                          _/
+
+                                 - find your next book -
+
+*********************************************************************************************
+Doc
+
 end
